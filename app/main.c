371de1b365588ea5e8ac53fab584c5e3dd53fc73
NikitaZhidov <nikita-zh8@mail.ru>
2020-03-31 17:03:32 +0300

    16.4

--- app/main.c
+++ app/main.c
@@ -13,7 +13,8 @@ int main()
     (void)scanf("%lf", &b);
     printf("Enter c: ");
     (void)scanf("%lf", &c);
-    double x1, x2;
+    double x1;
+    double x2;
     int count;
     count = quad_e(a, b, c, &x1, &x2);
     printf("x1 = %lf  x2 = %lf\n", x1, x2);
