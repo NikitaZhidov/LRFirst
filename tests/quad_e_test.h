83e494cf9618580df1af0850c4390fbaaaef1b67
NikitaZhidov <nikita-zh8@mail.ru>
2020-03-31 16:45:53 +0300

    16th commit

--- tests/quad_e_test.h
+++ tests/quad_e_test.h
@@ -8,35 +8,40 @@ extern "C" {
 }
 
 TEST(quad_eTest_p, positive1) {
-    double x1, x2;
+    double x1;
+    double x2;
     ASSERT_EQ(quad_e(1, 3, -4, &x1, &x2), 2);
     ASSERT_EQ(x1, 1);
     ASSERT_EQ(x2, -4);
 }
 
 TEST(quad_eTest_n, negative1){
-    double x1, x2;
+    double x1;
+    double x2;
     ASSERT_EQ(quad_e(0, 1, 1, &x1, &x2), 1);
     ASSERT_EQ(x1, -1);
     ASSERT_EQ(x2, 0);
 }
 
 TEST(quad_eTest_n, negative2){
-    double x1, x2;
+    double x1;
+    double x2;
     ASSERT_EQ(quad_e(0, 0, 5, &x1, &x2), 0);
     ASSERT_EQ(x1, 0);
     ASSERT_EQ(x2, 0);
 }
 
 TEST(quad_eTest_n, negative3){
-    double x1, x2;
+    double x1;
+    double x2;
     ASSERT_EQ(quad_e(-2, 0, 0, &x1, &x2), 1);
     ASSERT_EQ(x1, 0);
     ASSERT_EQ(x2, 0);
 }
 
 TEST(quad_eTest_n, negativ4){
-    double x1, x2;
+    double x1;
+    double x2;
     ASSERT_EQ(quad_e(1, 3, 4, &x1, &x2), 0);
     ASSERT_EQ(x1, 0);
     ASSERT_EQ(x2, 0);
